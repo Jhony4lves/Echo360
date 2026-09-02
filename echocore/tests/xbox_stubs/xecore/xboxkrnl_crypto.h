5add4106c15a2836abbccc31e5bde161d7b871ee
Jhony4lves <jhonyr.rocha@gmail.com>
2026-09-02 16:36:39 -0300

    test(echocore): align crypto stub with pinned xecorelib

--- echocore/tests/xbox_stubs/xecore/xboxkrnl_crypto.h
+++ echocore/tests/xbox_stubs/xecore/xboxkrnl_crypto.h
@@ -3,6 +3,8 @@
 
 #include "xboxkrnl_types.h"
 
+void XeCryptRandom(uint8_t *random, uint32_t random_size);
+
 void XeCryptSha256Init(CRYPT_SHA256_STATE *state);
 void XeCryptSha256Update(
     CRYPT_SHA256_STATE *state,
@@ -13,6 +15,16 @@ void XeCryptSha256Final(
     CRYPT_SHA256_STATE *state,
     uint8_t *digest,
     uint32_t digest_bytes
+);
+void XeCryptSha256(
+    const uint8_t *input1,
+    uint32_t input1_size,
+    const uint8_t *input2,
+    uint32_t input2_size,
+    const uint8_t *input3,
+    uint32_t input3_size,
+    uint8_t *digest,
+    uint32_t digest_size
 );
 
 #endif
